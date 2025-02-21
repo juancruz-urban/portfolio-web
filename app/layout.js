e25bcdade111d8bc60acf8bf186081ebe3c6e3b0
juan_cruz <urbanjuancruz@gmail.com>
2025-02-21 14:24:59 -0300

    add metadata

--- app/layout.js
+++ app/layout.js
@@ -5,7 +5,7 @@ import Header from '@/components/Header'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Portfolio de Juan Cruz Urban',
+  title: 'Juan Cruz Urban',
   description: 'Desarrollador Web Fullstack',
 }
 
